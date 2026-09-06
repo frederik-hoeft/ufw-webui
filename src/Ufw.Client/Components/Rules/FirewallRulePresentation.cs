3b162bde9f77c6b56f49eee100f824a43f5e09bc
frederik-hoeft <43813142+frederik-hoeft@users.noreply.github.com>
2026-09-06 13:52:47 +0200

    rule table UX improvements + refactor SCSS into hierarchy

--- src/Ufw.Client/Components/Rules/FirewallRulePresentation.cs
+++ src/Ufw.Client/Components/Rules/FirewallRulePresentation.cs
@@ -32,5 +32,44 @@ internal static class FirewallRulePresentation
         return value;
     }
 
+    public static string? DescribePorts(FirewallRuleSpecification rule)
+    {
+        ArgumentNullException.ThrowIfNull(rule);
+
+        bool hasSource = !string.IsNullOrWhiteSpace(rule.SourcePorts);
+        bool hasDestination = !string.IsNullOrWhiteSpace(rule.DestinationPorts);
+        return (hasSource, hasDestination) switch
+        {
+            (false, false) => null,
+            (false, true) => rule.DestinationPorts,
+            (true, false) => $"src {rule.SourcePorts}",
+            (true, true) => $"src {rule.SourcePorts} / dst {rule.DestinationPorts}",
+        };
+    }
+
+    public static string ActionClass(FirewallAction action) => action switch
+    {
+        FirewallAction.Allow => "rule-action rule-action-allow",
+        FirewallAction.Deny => "rule-action rule-action-deny",
+        FirewallAction.Reject => "rule-action rule-action-reject",
+        FirewallAction.Limit => "rule-action rule-action-limit",
+        _ => "rule-action",
+    };
+
+    public static string FormatAddressFamily(FirewallAddressFamily family) => family switch
+    {
+        FirewallAddressFamily.IPv4 => "IPv4",
+        FirewallAddressFamily.IPv6 => "IPv6",
+        _ => "Any family",
+    };
+
+    public static string FormatProtocol(FirewallProtocol protocol) => protocol switch
+    {
+        FirewallProtocol.Any => "any",
+        FirewallProtocol.Tcp => "tcp",
+        FirewallProtocol.Udp => "udp",
+        _ => protocol.ToString(),
+    };
+
     public static string FormatEnum<T>(T value) where T : struct, Enum => value.ToString();
 }
